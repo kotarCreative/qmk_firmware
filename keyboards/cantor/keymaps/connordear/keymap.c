684a29827d0aabf3204f0968c14c8ee92fd286dc
Connor Dear <connordear@gmail.com>
2023-05-27 17:16:20 -0600

    added initial miryoku config for cantor

--- keyboards/cantor/keymaps/connordear/keymap.c
+++ keyboards/cantor/keymaps/connordear/keymap.c
@@ -5,6 +5,7 @@
 #include QMK_KEYBOARD_H
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+<<<<<<< Updated upstream
      /*
       * ┌───┬───┬───┬───┬───┬───┐       ┌───┬───┬───┬───┬───┬───┐
       * │Tab│ Q │ W │ E │ R │ T │       │ Y │ U │ I │ O │ P │Bsp│
@@ -24,6 +25,51 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                               KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                               KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
                                             KC_LGUI, KC_SPC, KC_BSPC,          KC_ENT, KC_SPC, KC_RALT
+=======
+
+
+    /*  COLEMAK-DH Layer
+      * ┌───┬───┬───┬───┬───┬───┐       ┌───┬───┬───┬───┬───┬───┐
+      * │ ~ │ Q │ W │ F │ P │ B │       │ J │ L │ U │ Y │ ' │   │
+      * ├───┼───┼───┼───┼───┼───┤       ├───┼───┼───┼───┼───┼───┤
+      * │   │ A │ R │ S │ T │ G │       │ M │ N │ E │ I │ O │   │
+      * ├───┼───┼───┼───┼───┼───┤       ├───┼───┼───┼───┼───┼───┤
+      * │   │ Z │ X │ C │ D │ V │       │ K │ H │ , │ . │ / │   │
+      * └───┴───┴───┴───┴───┴───┘       └───┴───┴───┴───┴───┴───┘
+      *               ┌───┐                   ┌───┐
+      *               │Esc├───┐           ┌───┤Del│
+      *               └───┤Spc├───┐   ┌───┤Bsp├───┘
+      *                   └───┤Tab│   │Ent├───┘
+      *                       └───┘   └───┘
+      */
+    [0] = LAYOUT_split_3x6_3(
+        KC_GRV, KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,                               KC_J,    KC_L,    KC_U,    KC_Y,    KC_QUOT,  -----,
+        ------, KC_A,    KC_R,    KC_S,    KC_T,    KC_G,                               KC_M,    KC_N,    KC_E,    KC_I,    KC_O,     -----,
+        ------, KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,                               KC_K,    KC_H,    KC_COMM, KC_DOT,  KC_SLSH,  -----,
+                                            KC_ESC, KC_SPC, KC_TAB,          KC_ENT, KC_BSPC, KC_DEL
+    ),
+
+     /* NUMBER LAYER
+      * ┌───┬───┬───┬───┬───┬───┐       ┌───┬───┬───┬───┬───┬───┐
+      * │   │ [ │ 7 │ 8 │ 9 │ ] │       │ J │ L │ U │ Y │ ' │   │
+      * ├───┼───┼───┼───┼───┼───┤       ├───┼───┼───┼───┼───┼───┤
+      * │   │ ; │ 4 │ 5 │ 6 │ = │       │ M │ N │ E │ I │ O │   │
+      * ├───┼───┼───┼───┼───┼───┤       ├───┼───┼───┼───┼───┼───┤
+      * │   │ ` │ 1 │ 2 │ 3 │ \ │       │ K │ H │ , │ . │ / │   │
+      * └───┴───┴───┴───┴───┴───┘       └───┴───┴───┴───┴───┴───┘
+      *               ┌───┐                   ┌───┐
+      *               │Esc├───┐           ┌───┤Del│
+      *               └───┤Spc├───┐   ┌───┤Bsp├───┘
+      *                   └───┤Tab│   │Ent├───┘
+      *                       └───┘   └───┘
+      */
+
+    [1] = LAYOUT_split_3x6_3(
+        KC_TAB,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,                               KC_J,    KC_L,    KC_U,    KC_Y,    KC_QUOT, KC_BSPC,
+        KC_LCTL, KC_A,    KC_R,    KC_S,    KC_T,    KC_G,                               KC_M,    KC_N,    KC_E,    KC_I,    KC_O,    KC_SLSH,
+        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,                               KC_K,    KC_H,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+                                            KC_ESC, KC_SPC, KC_TAB,          KC_ENT, KC_BSPC, KC_DEL
+>>>>>>> Stashed changes
     )
 };
 
