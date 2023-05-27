6625bbe46a5b08cf254b0738df031221049aea79
Connor Dear <connordear@gmail.com>
2023-05-27 12:43:38 -0600

    gmmk builds

---
+++ keyboards/gmmk/pro/rev1/ansi/keymaps/connordear/config.h
@@ -0,0 +1,8 @@
+// Configure the global tapping term
+#define TAPPING_TERM 170
+
+// Prevent normal rollover from accidentally triggering mods.
+#define IGNORE_MOD_TAP_INTERRUPT
+
+// Enable rapid switch from tap to hold, disables double tab hold auto-repeat
+#define QUICK_TAP_TERM 0
